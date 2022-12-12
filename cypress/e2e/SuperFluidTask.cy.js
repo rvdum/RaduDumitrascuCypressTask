0ea46add67b5c917c4805b7278c3d735fa333612
rvdum <116432332+rvdum@users.noreply.github.com>
2022-12-12 18:52:53 +0200

    Task 1 & Task 2
    adding code for the first 2 tasks

---
+++ cypress/e2e/SuperFluidTask.cy.js
@@ -0,0 +1,34 @@
+import {BasePage} from "../pageObjects/BasePage";
+
+describe("Cypress Task Test Cases", () => {
+
+  it("Access the webpage and Select the Goerli network", () => {
+    //To get a wallet connected during the test cases , use visitWithMockedWallet() function instead of normal cy.visit()
+    //Otherwise you won't be able to automate the steps or see the error messages
+    BasePage.visitWithMockedWallet("/")
+    BasePage.openNetworkSelectionMenu()
+    BasePage.openTestnetsList()
+    BasePage.selectGoerliNetwork()
+  })
+
+  it("Connect to the Mock wallet", () => {
+    BasePage.visitWithMockedWallet("/")
+    BasePage.openNetworkSelectionMenu()
+    BasePage.openTestnetsList()
+    BasePage.selectGoerliNetwork()
+    BasePage.accessConnectToWalletSection()
+    BasePage.selectMockWallet()
+
+  })
+  it.only ("Attempt to Claim Tokens from the Dashboard and validate error message", () => {
+    BasePage.visitWithMockedWallet("/")
+    BasePage.openNetworkSelectionMenu()
+    BasePage.openTestnetsList()
+    BasePage.selectGoerliNetwork()
+    BasePage.accessConnectToWalletSection()
+    BasePage.selectMockWallet()
+    BasePage.goToClaim()
+    BasePage.attemptToClaimTokens()
+    BasePage.validateErrorMessage("Something went wrong, please try again")
+  })
+})
--- cypress/pageObjects/BasePage.js
+++ cypress/pageObjects/BasePage.js
@@ -1,22 +1,84 @@
 import HDWalletProvider from "@truffle/hdwallet-provider";
 import {ethers} from "ethers";
 
+const NETWORK_SELECTION_BUTTON ="[data-cy=top-bar-network-button]"
+const TESTNETS_LIST_BUTTON ="[data-cy=testnets-button]"
+const GOERLI_BUTTON ="[data-cy=goerli-button]"
+const CONNECT_WALLET_BUTTON = ".css-xhnfv3 > [data-cy=connect-wallet-button]"
+const MOCK_WALLET = "[data-testid=rk-wallet-option-mock]"
+const CONNECTED_MESSAGE = ".MuiTypography-body2"
+const OPEN_CLAIM_SECTION = ".MuiListItemSecondaryAction-root > .MuiButtonBase-root"
+const ERROR_MESSAGE_CLAIM_TOKENS = ".MuiAlert-message > .MuiTypography-root"
+
 export class BasePage {
 
     static visitWithMockedWallet(page) {
         cy.visit(page, {
             onBeforeLoad: (win) => {
                 const hdwallet = new HDWalletProvider({
-                    privateKeys: ["INSERT PRIVATE KEY HERE"],
+                    privateKeys: ["4096b4d75e5351653841c31068644742c63f947382461748c2b7823ca971c237"],
                     url: "https://rpc-endpoints.superfluid.dev/eth-goerli",
                     chainId: 5,
                     pollingInterval: 1000,
                 });
                 win.mockSigner = new ethers.providers.Web3Provider(hdwallet).getSigner();
             }
-
         })
+    }
 
+    // Making my life easier
+    static click(selector) {
+        cy.get(selector).click()
+    }
+
+    static type(selector, message) {
+        cy.get(selector).type(message)
+    }
+
+    static isVisible(selector) {
+        cy.get(selector).should("be.visible")
+    }
+
+    static hasText(selector, text) {
+        cy.get(selector).should('have.text', text).and('be.visible')
+    }
+
+    // Making my life easier ^
+
+    static openNetworkSelectionMenu() {
+        this.click(NETWORK_SELECTION_BUTTON)
+
+    }
+
+    static openTestnetsList() {
+       this.click(TESTNETS_LIST_BUTTON)
+
+    }
+
+   static selectGoerliNetwork() {
+        this.click(GOERLI_BUTTON)
+    }
+
+    static accessConnectToWalletSection() {
+        this.click(CONNECT_WALLET_BUTTON)
+    }
+
+    static selectMockWallet() {
+        this.click(MOCK_WALLET)
+        cy.get(CONNECTED_MESSAGE).should("be.visible")
+    }
+
+    static goToClaim() {
+        this.click(OPEN_CLAIM_SECTION)
+    }
+
+    static attemptToClaimTokens() {
+        cy.wait(5000)
+        cy.get(".MuiDialogContent-root").contains("Claim").click()
+    }
+
+    static validateErrorMessage(message) {
+        this.hasText(ERROR_MESSAGE_CLAIM_TOKENS,message)
     }
 }
 
